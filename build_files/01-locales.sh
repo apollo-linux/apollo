bb3e2a7f9ae8561c10d3845f206f8615f4da17d7
Isabelle Jackson <izzyjackson05@outlook.com>
2025-12-14 02:17:31 +0000

    fix: set default local to en_US

--- build_files/01-locales.sh
+++ build_files/01-locales.sh
@@ -6,5 +6,6 @@ sed -i 's/^NoExtract/#NoExtract/' /etc/pacman.conf
 pacman -Sy --noconfirm glibc
 
 echo "Enabling locales"
+echo "LANG=en_US.UTF-8" >> /etc/locale.conf
 sed -i 's/^#\(.*\.UTF-8\)/\1/' /etc/locale.gen
 locale-gen
