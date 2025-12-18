15439d981ef6304441fec1d02df15cbaa38ca89b
Isabelle Jackson <izzyjackson05@outlook.com>
2025-12-18 20:21:40 +0000

    fix: remove unused noextract

--- build_files/01-locales.sh
+++ build_files/01-locales.sh
@@ -2,8 +2,8 @@
 
 set -ouex pipefail
 
-sed -i 's/^NoExtract/#NoExtract/' /etc/pacman.conf
-pacman -Sy --noconfirm glibc glibc-locales
+echo "Install glibc-locales"
+pacman -Sy --noconfirm glibc-locales
 
 echo "Setting the default locale..."
 echo "LANG=en_US.UTF-8" >> /etc/locale.conf
