766e4b665aa78cfd3b21b1b60c610b3c0ca5d1de
Isabelle Jackson <izzyjackson05@outlook.com>
2026-02-13 13:55:44 +0000

    chore: add more of the gnome group

--- build_files/gnome/01-gnome.sh
+++ build_files/gnome/01-gnome.sh
@@ -4,6 +4,7 @@ set -ouex pipefail
 
 echo "Installing GNOME..."
 
+# Install a subset of the gnome package group without most of the apps
 pacman -S --noconfirm \
     gnome-shell \
     gnome-session \
@@ -12,13 +13,31 @@ pacman -S --noconfirm \
     xdg-desktop-portal-gnome \
     gnome-control-center \
     gnome-backgrounds \
-    gnome-keyring
+    gnome-keyring \
+    gnome-user-share \
+    gnome-remote-desktop \
+    rygel \
+    tecla \
+    grilo-plugins
 
 # Install GNOME applications
 pacman -S --noconfirm \
     nautilus \
     ptyxis \
     gnome-disk-utility
+
+# Install GVFS Plugins
+pacman -S --noconfirm \
+    gvfs-afc \
+    gvfs-dnssd \
+    gvfs-goa \
+    gvfs-google \
+    gvfs-gphoto2 \
+    gvfs-mtp \
+    gvfs-nfs \
+    gvfs-onedrive \
+    gvfs-smb \
+    gvfs-wsdd \
 
 # Install Acessibility Tools
 pacman -S --noconfirm \
